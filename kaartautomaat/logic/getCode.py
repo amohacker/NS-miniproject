65ce01bf996b3a06d8932f9bca33398e039f0f55
amohacker <tnvtablet@gmail.com>
2021-01-30 19:08:38 +0100

    Zoekfuncie is niet langer case sensitief

--- kaartautomaat/logic/getCode.py
+++ kaartautomaat/logic/getCode.py
@@ -7,7 +7,7 @@ def getCode(station):
     for i in stations:
         for naam in i['namen'].values():
             # als station met de juiste naam gevonden word word deze gereturned
-            if (naam == station):
+            if naam.lower() == station.lower():
                 return i['code']
     # als het station niet gevonden kon worden word er een exception geraised
     raise Exception('Station {} kon niet gevonden worden'.format(station))
